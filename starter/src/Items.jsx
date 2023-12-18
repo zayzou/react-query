466662338e7cd08b2f0e66abaf427f925601531f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-18 11:49:36 -0500

    Errors and interceptors

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -3,16 +3,25 @@ import { useQuery } from "@tanstack/react-query";
 import httpClient from "./utils";
 
 const Items = () => {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, isError, error } = useQuery({
     queryKey: ["tasks"],
     queryFn: async () => {
-      const { data } = await httpClient.get("");
+      const { data } = await httpClient.get("/stuff");
       return data;
     },
   });
 
   if (isLoading) {
     return <h4 style={{ marginTop: "2rem" }}>Loading...</h4>;
+  }
+  // if (isError) {
+  //   return (
+  //     <h4 style={{ marginTop: "2rem", color: "red" }}>An error occurred</h4>
+  //   );
+  // }
+
+  if (error) {
+    return <h4 style={{ marginTop: "2rem", color: "red" }}>{error.message}</h4>;
   }
 
   return (
--- starter/src/utils.js
+++ starter/src/utils.js
@@ -3,5 +3,23 @@ import axios from "axios";
 const httpClient = axios.create({
   baseURL: "http://localhost:8080/api/tasks",
 });
+/* httpClient.interceptors.request.use(
+  (request) => {
+    return request;
+  },
+  (error) => {
+    console.log("Something went wrong");
+    Promise.reject(error);
+  }
+);
 
+httpClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log("Something went wrong");
+    Promise.reject(error);
+  }
+); */
 export default httpClient;
