56c828a8473ca1d81fe8d5f4bb2db4ae8f426ca7
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-18 11:15:51 -0500

    Setup React Query

--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
